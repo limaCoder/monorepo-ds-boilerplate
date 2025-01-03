a906b497f857af80767bed11c206f54c340d441c
Mario Augusto de Lima <marioaulima15@outlook.com>
2025-01-03 17:44:04 -0300

    feat: improving home page and create delivery validation guide

---
+++ apps/docs/src/pages/components/components/QuickLinkCard/types.ts
@@ -0,0 +1,12 @@
+import { ElementType } from 'react';
+
+interface QuickLinkCardProps {
+  icon: ElementType;
+  iconColor?: string;
+  title: string;
+  description: string;
+  linkText: string;
+  href: string;
+}
+
+export type { QuickLinkCardProps };
